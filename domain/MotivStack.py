d6ab0e9a72224ecf3da2353b25d02eaa51d16f50
ElizaDie <marszalik@gmail.com>
2025-07-08 09:50:19 +0000

    Dodamy MotivStack

---
+++ domain/MotivStack.py
@@ -0,0 +1,26 @@
+from services.Singleton import Singleton
+
+
+@Singleton
+class MotivStack:
+
+  def __init__(self):
+    self.stack = []
+
+  def addMotivation(self, motivation):
+    self.stack.append(motivation)
+
+  def show(self):
+    print("MotivStack:")
+    print(f"Stack size: {len(self.stack)}")
+    print("Motivations in the stack:")
+    for motivation in self.stack:
+      motivation.show()
+      print("\t----------\n")
+
+  def getFormated(self):
+    prompt = ""
+    for motivation in self.stack:
+      prompt += motivation.getFormated()
+      prompt += "\n\n---\n\n"
+      return prompt
